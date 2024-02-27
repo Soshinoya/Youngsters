a35a56a8706fc94228194d8a8880becaf5da82b1
Alex <nike95125@gmail.com>
2024-02-27 11:37:51 +0300

    added menu-item and menu-item-dropdown components

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+/*
+• Вешаем обработчик события клика на Menu-item-dropdown
+• и при клике получаем значение data-dropdown-target
+• ищем список по этому значению
+• и добавляем специальный класс этому списку и активный класс на сам dropdownElem
+*/
+
+const menuItemDropdowns = document.querySelectorAll('.menu-item-dropdown')
+
+menuItemDropdowns.forEach(dropdownElem => {
+    dropdownElem.addEventListener('click', e => {
+        const dropdownTargetValue = e.target.getAttribute('data-dropdown-target')
+        const dropdownTargetList = document.querySelector(`[data-dropdown-list="${dropdownTargetValue}"]`)
+        dropdownElem.classList.toggle('menu-item-dropdown--active')
+        dropdownTargetList.classList.toggle('menu-dropdown-list--visible')
+    })
+})
