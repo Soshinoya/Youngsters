90c860feffdb25dda931d515efc87271aa9d5818
Alex <nike95125@gmail.com>
2024-02-28 13:59:01 +0300

    adaprtive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+const transitionDuration = 400;
+
 const setPaddingLeftFromContainerToElem = elem => {
     if (!elem) return
     const container = document.querySelector('.container')
@@ -5,17 +7,47 @@ const setPaddingLeftFromContainerToElem = elem => {
 }
 
 window.addEventListener('resize', () => {
-    setPaddingLeftFromContainerToElem(document.querySelector('aside.header-sidebar'))
+    !window.matchMedia('(max-width: 320px)').matches && setPaddingLeftFromContainerToElem(document.querySelector('aside.header-sidebar'))
 })
 
-setPaddingLeftFromContainerToElem(document.querySelector('aside.header-sidebar'))
+!window.matchMedia('(max-width: 320px)').matches && setPaddingLeftFromContainerToElem(document.querySelector('aside.header-sidebar'))
+
+const toggleHeaderSidebarInnerPage = action => {
+    const headerSidebarTop = document.querySelector('.header-sidebar-top')
+    const headerSidebarMainPage = document.querySelector('.header-sidebar-main-page')
+    const headerSidebarInnerPage = document.querySelector('.header-sidebar-inner-page')
+
+    headerSidebarTop.classList.toggle('header-sidebar-top__inner-page')
+    if (action === 'close') {
+        headerSidebarInnerPage.classList.remove('header-sidebar-inner-page--visible')
+        setTimeout(() => {
+            headerSidebarMainPage.classList.remove('d-none')
+            headerSidebarMainPage.classList.remove('header-sidebar-main-page--hidden')
+        }, transitionDuration)
+    } else {
+        headerSidebarMainPage.classList.add('header-sidebar-main-page--hidden')
+        setTimeout(() => {
+            headerSidebarMainPage.classList.add('d-none')
+            headerSidebarInnerPage.classList.add('header-sidebar-inner-page--visible')
+        }, transitionDuration)
+
+    }
+}
 
 document.addEventListener('click', e => {
     // Toggle dropdowns
-    const dropdownElem = e.target.closest('.menu-item-dropdown')
+    // const dropdownElem = e.target.closest('.menu-item-dropdown')
+    const dropdownElem = e.target.closest('[data-dropdown-target]')
     if (dropdownElem) {
         const dropdownTargetValue = dropdownElem.getAttribute('data-dropdown-target')
         const dropdownTargetList = document.querySelector(`[data-dropdown-list="${dropdownTargetValue}"]`)
+
+        if (window.matchMedia('(max-width: 480px)').matches) {
+            if (dropdownTargetValue === 'header-sidebar-catalog') {
+                toggleHeaderSidebarInnerPage('open')
+                return
+            }
+        }
 
         dropdownElem.classList.toggle('menu-item-dropdown--active')
         dropdownTargetList?.classList.toggle(dropdownTargetValue === 'header-sidebar' ? 'header-sidebar--visible' : 'menu-dropdown-list--visible')
@@ -27,6 +59,9 @@ document.addEventListener('click', e => {
         const headerSidebar = document.querySelector('.header-sidebar')
         headerSidebarLinkDropdown?.classList.remove('menu-item-dropdown--active')
         headerSidebar?.classList.remove('header-sidebar--visible')
+
+        // If an internal page is open - close it too
+        if (document.querySelector('.header-sidebar-top__inner-page')) toggleHeaderSidebarInnerPage('close')
     }
 
     // Close dropdowns with data-dropdown-close attribute
@@ -38,6 +73,9 @@ document.addEventListener('click', e => {
 
         dropdownTarget?.classList.remove('menu-item-dropdown--active')
         dropdownList?.classList.remove(dropdownCloseValue === 'header-sidebar' ? 'header-sidebar--visible' : 'menu-dropdown-list--visible')
+
+        // If an internal page is open - close it too
+        if (document.querySelector('.header-sidebar-top__inner-page')) toggleHeaderSidebarInnerPage('close')
     }
 
     // Opening / Closing search-overlay in the header
@@ -50,7 +88,10 @@ document.addEventListener('click', e => {
             searchOverlay.classList.remove('search-overlay--visible')
             searchBtn.classList.remove('header-links__search--active')
         }
-    } else if (e.target.closest('.header-links__search')) {
+    } else if (
+        e.target.closest('.header-links__search')
+        || e.target.closest('.header-nav__search')
+    ) {
         header.classList.add('header-search')
         searchOverlay.classList.add('search-overlay--visible')
         searchBtn.classList.add('header-links__search--active')
