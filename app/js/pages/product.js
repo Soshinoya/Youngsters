1eac980b62a03f511cd43fefdf14fd5241f60094
Alex <nike95125@gmail.com>
2024-03-05 19:00:33 +0300

    added goods slider

--- app/js/pages/product.js
+++ app/js/pages/product.js
@@ -1,9 +1,22 @@
 new Swiper('.product-images-slider .swiper', {
-    // direction: 'vertical',
     slidesPerView: 1,
     autoplay: true,
     pagination: {
         el: '.product-images-slider__pagination.swiper-pagination',
         clickable: true,
+    }
+})
+
+new Swiper('.goods-slider .swiper', {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    spaceBetween: 6,
+    pagination: {
+        el: '.goods-slider .swiper-pagination',
+        type: 'fraction'
+    },
+    navigation: {
+        nextEl: '.goods-slider .swiper-button-next',
+        prevEl: '.goods-slider .swiper-button-prev'
     }
 })
