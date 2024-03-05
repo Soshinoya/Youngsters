2c6ccea66c2da3dcf37ec39cc8917fc91c70283a
Alex <nike95125@gmail.com>
2024-03-05 21:10:02 +0300

    added product-social section, adaptive product-social and goods-slider sections

--- app/js/pages/product.js
+++ app/js/pages/product.js
@@ -7,16 +7,41 @@ new Swiper('.product-images-slider .swiper', {
     }
 })
 
-new Swiper('.goods-slider .swiper', {
-    slidesPerView: 4,
-    slidesPerGroup: 4,
-    spaceBetween: 6,
-    pagination: {
-        el: '.goods-slider .swiper-pagination',
-        type: 'fraction'
-    },
-    navigation: {
-        nextEl: '.goods-slider .swiper-button-next',
-        prevEl: '.goods-slider .swiper-button-prev'
+const setGoodsSlider = () => {
+    if (window.matchMedia('(min-width: 1025px)').matches) {
+        new Swiper('.goods-slider .swiper', {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            spaceBetween: 6,
+            navigation: { nextEl: '.goods-slider .swiper-button-next', prevEl: '.goods-slider .swiper-button-prev' },
+            pagination: { el: '.goods-slider-pagination--md.swiper-pagination', type: 'fraction' }
+        })
+    } else if (window.matchMedia('(min-width: 769px)').matches) {
+        new Swiper('.goods-slider .swiper', {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            spaceBetween: 6,
+            navigation: { nextEl: '.goods-slider .swiper-button-next', prevEl: '.goods-slider .swiper-button-prev' },
+            pagination: { el: '.goods-slider-pagination--md.swiper-pagination', type: 'fraction' }
+        })
+    } else if (window.matchMedia('(min-width: 415px)').matches) {
+        new Swiper('.goods-slider .swiper', {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 6,
+            navigation: { nextEl: '.goods-slider .swiper-button-next', prevEl: '.goods-slider .swiper-button-prev' },
+            pagination: { el: '.goods-slider-pagination--md.swiper-pagination', type: 'fraction' }
+        })
+    } else {
+        new Swiper('.goods-slider .swiper', {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 6,
+            pagination: { el: '.goods-slider-pagination--sm.swiper-pagination', clickable: true }
+        })
     }
-})
+}
+
+window.addEventListener('resize', setGoodsSlider)
+
+setGoodsSlider()
