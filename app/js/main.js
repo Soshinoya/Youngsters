2afaa61d3e4419105abaa05fbeb614fba5942362
Soshinoya <nike95125@gmail.com>
2024-03-14 19:08:38 +0300

    added guest-favorites page

--- app/js/main.js
+++ app/js/main.js
@@ -312,6 +312,46 @@ breadcrumbsInner?.addEventListener('wheel', e => {
     breadcrumbsInner.scrollLeft += e.deltaY
 })
 
+// Goods Slider
+const setGoodsSlider = () => {
+    if (window.matchMedia('(min-width: 1025px)').matches) {
+        new Swiper('.goods-slider .swiper', {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            spaceBetween: 6,
+            navigation: { nextEl: '.goods-slider .swiper-button-next', prevEl: '.goods-slider .swiper-button-prev' },
+            pagination: { el: '.goods-slider-pagination--md.swiper-pagination', type: 'fraction' }
+        })
+    } else if (window.matchMedia('(min-width: 769px)').matches) {
+        new Swiper('.goods-slider .swiper', {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            spaceBetween: 6,
+            navigation: { nextEl: '.goods-slider .swiper-button-next', prevEl: '.goods-slider .swiper-button-prev' },
+            pagination: { el: '.goods-slider-pagination--md.swiper-pagination', type: 'fraction' }
+        })
+    } else if (window.matchMedia('(min-width: 415px)').matches) {
+        new Swiper('.goods-slider .swiper', {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 6,
+            navigation: { nextEl: '.goods-slider .swiper-button-next', prevEl: '.goods-slider .swiper-button-prev' },
+            pagination: { el: '.goods-slider-pagination--md.swiper-pagination', type: 'fraction' }
+        })
+    } else {
+        new Swiper('.goods-slider .swiper', {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 6,
+            pagination: { el: '.goods-slider-pagination--sm.swiper-pagination', clickable: true }
+        })
+    }
+}
+
+window.addEventListener('resize', setGoodsSlider)
+
+setGoodsSlider()
+
 window.addEventListener('resize', () => {
     console.clear()
     console.log(window.innerWidth)
